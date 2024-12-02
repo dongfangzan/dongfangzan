bc33ee2089c131a57dadf8a2bd45a5fe95f5e165
dongfangzan <787814104@qq.com>
2024-12-02 14:52:54 +0800

    忽略错误

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'dongfangzan', // Usually your GitHub org/user name.
   projectName: 'dongfangzan', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
